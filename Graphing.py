7ee1a0ead8a4658c0ffb0274f241ba45c33cb517
Thanish24 <boggulathanish@gmail.com>
2024-03-29 19:23:05 -0400

    updated DAL data and got code to plot financial data

--- Graphing.py
+++ Graphing.py
@@ -1,19 +1,25 @@
-import matplotlib.pyplot as plt 
-import csv 
-  
-x = [] 
-y = [] 
-  
-with open('biostats.csv','r') as csvfile: 
-    plots = csv.reader(csvfile, delimiter = ',') 
-      
-    for row in plots: 
-        x.append(row[0]) 
-        y.append(int(row[2])) 
-  
-plt.bar(x, y, color = 'g', width = 0.72, label = "Age") 
-plt.xlabel('Names') 
-plt.ylabel('Ages') 
-plt.title('Ages of different persons') 
-plt.legend() 
-plt.show() 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Create a pandas DataFrame from the data
+crudeOilDf = pd.read_csv('NoSinDaWooQuant/CLF_data.csv')
+
+deltaDf = pd.read_csv('NoSinDaWooQuant/DAL_data.csv')
+
+# Convert 'Date' column to datetime type
+crudeOilDf['Date'] = pd.to_datetime(crudeOilDf['Date'])
+deltaDf['Date'] = pd.to_datetime(deltaDf['Date'])
+
+# Plotting
+plt.figure(figsize=(10, 6))
+plt.plot(crudeOilDf['Date'], crudeOilDf['Close'], marker='o', linestyle='-', label='crude oil')
+plt.plot(deltaDf['Date'], deltaDf['Close'], marker='o', linestyle='-', label='delta')
+plt.title('Crude Oil and Delta Close Price Over Time')
+plt.xlabel('Date')
+plt.ylabel('Close Price')
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.legend()
+
+plt.show()
